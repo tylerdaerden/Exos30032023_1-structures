42465dced105e63bc74be00fb73357931bdbcd38
Tyler Daerden <daerdentyler@gmail.com>
2023-03-30 14:11:01 +0200

    Ajoutez des fichiers projet.

---
+++ Exos_Structure/Program.cs
@@ -0,0 +1,46 @@
+﻿#region Exo_Structure
+
+#region 1.1 Jeu de Cartes
+
+// Définir un tableau d'entier contenant les valeurs comprises entre 1 et 13 inclus
+// Définir un tableau de string contenant les valeurs "Coeur" , "Carreau" , "Pique" et "Trefle"
+// Définir une structure Carte contenant les 2 variables suivantes public int Valeur ; public string Couleur , 
+// Sur base des tableaux Valeurs et Couleurs, définis précédemment, générez le jeu de 52 cartes et leur affecter leur Couleur et leurs valeurs
+// Ensuite mélanger le jeu de Carte.
+//Enfin afficher chacune des cartes du jeu style " 1 de Coeur" , etc ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endregion
+
+#endregion
